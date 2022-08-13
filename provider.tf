38596fe884bb6d0fbef4036c3d51b251f96b4b61
tanveersrajpal <tanveersrajpal@gmail.com>
2022-08-14 03:28:47 +1200

    added backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.AWS_Region
 }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "clearpoint-tfstate-backend"
+    key            = "state/terraform.tfstate"
+    region         = "ap-southeast-2"
+  }
+}
